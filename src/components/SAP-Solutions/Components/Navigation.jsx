b6dc9af91b7e0942877054b11f868212e6bc300e
Ashwin Kandathil Biju <akandathilbiju1@sheffield.ac.uk>
2025-03-21 20:31:25 +0000

    Design change in Navigation bar in SAP services component

--- src/components/SAP-Solutions/Components/Navigation.jsx
+++ src/components/SAP-Solutions/Components/Navigation.jsx
@@ -18,6 +18,7 @@ const Navigation = () => {
           "insights",
           "benefits",
           "faq",
+          "otherservices",
         ];
         let foundSection = "";
 
@@ -55,29 +56,32 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="hidden md:block sticky top-16 lg:top-14 z-40 bg-slate-100 shadow-md border-b">
-      <ul className="flex justify-center space-x-8 py-2 text-md font-medium text-gray-700">
-        {[
-          { id: "overview", label: "Overview" },
-          { id: "offerings", label: "Offerings" },
-          { id: "insights", label: "Insights" },
-          { id: "benefits", label: "Benefits" },
-          { id: "faq", label: "FAQ" },
-        ].map((item) => (
-          <li key={item.id}>
-            <button
-              onClick={() => scrollToSection(item.id)}
-              className={`px-4 py-1 transition-all duration-300 ${
-                activeSection === item.id
-                  ? "text-blue-900 border-blue-900"
-                  : " hover:text-blue-700"
-              }`}
-            >
-              {item.label}
-            </button>
-          </li>
-        ))}
-      </ul>
+    <nav className="hidden md:block sticky top-16 lg:top-16 z-40 pt-4">
+      <div className="max-w-3xl mx-auto bg-slate-100 rounded-lg shadow-md border border-slate-200">
+        <ul className="flex justify-center space-x-8 py-2 text-md font-medium text-gray-700">
+          {[
+            { id: "overview", label: "Overview" },
+            { id: "offerings", label: "Offerings" },
+            { id: "insights", label: "Insights" },
+            { id: "benefits", label: "Benefits" },
+            { id: "faq", label: "FAQ" },
+            { id: "otherservices", label: "Other Services" },
+          ].map((item) => (
+            <li key={item.id}>
+              <button
+                onClick={() => scrollToSection(item.id)}
+                className={`px-4 py-1 transition-all duration-300 ${
+                  activeSection === item.id
+                    ? "text-blue-900 border-blue-900"
+                    : "hover:text-blue-700"
+                }`}
+              >
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
--- src/components/SAP-Solutions/SolutionDetail.jsx
+++ src/components/SAP-Solutions/SolutionDetail.jsx
@@ -37,7 +37,7 @@ const SolutionDetail = ({ data }) => {
         <FAQ data={data.faqSection} />
       </section>
 
-      <section>
+      <section id="otherservices">
         <OtherServices navLinks={navLinks} />
       </section>
 
